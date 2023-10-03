5be0422c98951d5bf5acd56f79782b722a6e28e8
Wajd Ali <wajd.ali.etu@univ-lille.com>
2023-10-03 11:59:31 +0200

    add my previous solutions

---
+++ rp_poetry/password4.py
@@ -0,0 +1,30 @@
+# Taken from mission Acceptable Password III
+
+# Taken from mission Acceptable Password II
+# Taken from mission Acceptable Password I
+def is_acceptable_password(password: str) -> bool:
+    cond1 = len(password) > 6
+    cond2 = any(map(str.isdigit, password))
+    cond3 = any(map(str.isalpha, password))
+    cond4 = len(password) > 9
+    if cond1 and ((cond2 and cond3) or cond4):
+        return True
+    else:
+        return False
+
+
+print("Example:")
+print(is_acceptable_password("short"))
+
+# These "asserts" are used for self-checking
+assert is_acceptable_password("short") == False
+assert is_acceptable_password("short54") == True
+assert is_acceptable_password("muchlonger") == True
+assert is_acceptable_password("ashort") == False
+assert is_acceptable_password("notshort") == False
+assert is_acceptable_password("muchlonger5") == True
+assert is_acceptable_password("sh5") == False
+assert is_acceptable_password("1234567") == False
+assert is_acceptable_password("12345678910") == True
+
+print("The mission is done! Click 'Check Solution' to earn rewards!")
