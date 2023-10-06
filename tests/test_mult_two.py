202b527825f8ede9e74c65c9538532d9b9fcb31a
Wajd Ali <wajd.ali.etu@univ-lille.com>
2023-10-06 14:47:08 +0200

    modified 2 test files

--- tests/test_mult_two.py
+++ tests/test_mult_two.py
@@ -2,6 +2,6 @@ from rp_poetry import mult_two as mt
 import pytest
 def test_mult_two():
 
-    assert mult_two(3, 2) == 6
-    assert mult_two(0, 1) == 0
+    assert mt.mult_two(3, 2) == 6
+    assert mt.mult_two(0, 1) == 0
 
--- tests/test_password1.py
+++ tests/test_password1.py
@@ -1,9 +1,9 @@
-import rp_poetry.password1
+import rp_poetry.password1 as p1
 
 def test_is_acceptable_password():
 
     # These "asserts" are used for self-checking
-    assert password1.is_acceptable_password("short") == False
-    assert password1.is_acceptable_password("muchlonger") == True
-    assert password1.is_acceptable_password("ashort") == False
+    assert p1.is_acceptable_password("short") == False
+    assert p1.is_acceptable_password("muchlonger") == True
+    assert p1.is_acceptable_password("ashort") == False
 
