ea60f6d6cf57a120790d3b95f9342ab4cc24bb04
WajdAli <127183061+WajdAli@users.noreply.github.com>
2023-10-20 09:41:51 +0200

    Check and fix Backward_String.py after the recommendations raised by the linter

--- rp_poetry/Backward_String.py
+++ rp_poetry/Backward_String.py
@@ -1,7 +1,17 @@
 def backward_string(val: str) -> str:
+    """
+    Reverse the given string.
+
+    Args:
+        val (str): The input string to reverse.
+
+    Returns:
+        str: The reversed string.
+    """
     return val[::-1]
 
-
-print("Example:")
-print(backward_string("val"))
-
+# Example usage:
+if __name__ == "__main__":
+    print("Example:")
+    reversed_str = backward_string("val")
+    print(reversed_str)
